6a29b011162724b551873c02e70dda17bc33e0f0
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-23 12:29:04 +0530

    feat: create User entity and UserRole enum

---
+++ src/main/java/com/technogise/leave_management_system/entity/User.java
@@ -0,0 +1,48 @@
+package com.technogise.leave_management_system.entity;
+
+import com.technogise.leave_management_system.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name",unique = true, length = 50)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(name="joining_date", nullable = false)
+    private Date joiningDate;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/technogise/leave_management_system/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.technogise.leave_management_system.enums;
+
+public enum UserRole {
+    EMPLOYEES,
+    MANAGER,
+    ADMIN
+}
