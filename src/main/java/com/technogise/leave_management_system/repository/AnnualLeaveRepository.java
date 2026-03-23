4de78cf2f58f2d1d432b8bb32f0ec1340ad4f72e
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-03-23 15:51:37 +0530

    fix: set AnnualLeave in place of Holiday in Repository:

--- src/main/java/com/technogise/leave_management_system/repository/AnnualLeaveRepository.java
+++ src/main/java/com/technogise/leave_management_system/repository/AnnualLeaveRepository.java
@@ -1,12 +1,12 @@
 package com.technogise.leave_management_system.repository;
 
-import com.technogise.leave_management_system.entity.Holiday;
+import com.technogise.leave_management_system.entity.AnnualLeave;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface AnnualLeaveRepository extends JpaRepository<Holiday,UUID>{
+public interface AnnualLeaveRepository extends JpaRepository<AnnualLeave,UUID>{
 
 }
