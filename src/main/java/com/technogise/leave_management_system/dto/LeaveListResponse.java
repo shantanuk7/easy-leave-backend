cbefd0d7fccc5838d12426b7212288f987fcc8ab
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-24 11:47:38 +0530

    feat(LMS-15): add LeaveListResponse DTO for leave listing

--- src/main/java/com/technogise/leave_management_system/dto/LeaveListResponse.java
+++ src/main/java/com/technogise/leave_management_system/dto/LeaveListResponse.java
@@ -1,11 +1,21 @@
 package com.technogise.leave_management_system.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.UUID;
 
-public class LeaveListResponseDto {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LeaveListResponse {
     public UUID id;
     public Date date;
     public String type;
     public String duration;
+    public String reason;
 }
