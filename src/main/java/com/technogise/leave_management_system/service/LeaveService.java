29f6454042d6a9f5fa3ad7f262e45e76ff55cf6e
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-24 17:29:27 +0530

    feat(LMS-15):filter list of leave based on status query parameter

--- src/main/java/com/technogise/leave_management_system/service/LeaveService.java
+++ src/main/java/com/technogise/leave_management_system/service/LeaveService.java
@@ -2,6 +2,7 @@ package com.technogise.leave_management_system.service;
 
 import com.technogise.leave_management_system.dto.LeaveResponse;
 import com.technogise.leave_management_system.entity.User;
+import com.technogise.leave_management_system.exception.InvalidQueryParameterException;
 import com.technogise.leave_management_system.exception.ResourceNotFoundException;
 import com.technogise.leave_management_system.repository.LeaveRepository;
 import com.technogise.leave_management_system.repository.UserRepository;
@@ -22,11 +23,12 @@ public class LeaveService {
     }
     public User findUserById(UUID id) {
         return userRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("USER_NOT_FOUND", "User not found with id: " + id));
+                () -> new ResourceNotFoundException("NOT_FOUND", "User not found with id: " + id));
     }
     public List<LeaveResponse> getAllLeaves(UUID userId, String status, UUID requestedEmployeeId) {
         User user = findUserById(userId);
-        List<LeaveResponse> leaveList= leaveRepository.findAllByUserId(userId, Sort.by(Sort.Direction.DESC, "createdAt"))
+        List<LeaveResponse> leaveList = leaveRepository
+                .findAllByUserId(userId, Sort.by(Sort.Direction.DESC, "createdAt"))
                 .stream()
                 .map(leave -> new LeaveResponse(
                         leave.getId(),
@@ -38,6 +40,20 @@ public class LeaveService {
                         leave.getUpdatedAt(),
                         leave.getDescription()
                 )).toList();
+        if(status != null) {
+            if ("upcoming".equalsIgnoreCase(status)) {
+                leaveList = leaveList.stream()
+                        .filter(leave -> leave.getDate() != null && leave.getDate().after(new Date()))
+                        .toList();
+            } else if ("completed".equalsIgnoreCase(status)) {
+                leaveList = leaveList.stream()
+                        .filter(leave -> leave.getDate() != null && leave.getDate().before(new Date()))
+                        .toList();
+            }
+            else {
+                throw new InvalidQueryParameterException("BAD_REQUEST","invalid status query parameter");
+            }
+        }
         return leaveList;
     }
 }
