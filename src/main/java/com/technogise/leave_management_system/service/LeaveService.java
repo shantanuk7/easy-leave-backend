a4a6889549fd8c2818e7f822dd6c18a139cf8c5a
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-24 16:17:12 +0530

    feat(LMS-15):implement getAllLeaves service with user validation and sorting

--- src/main/java/com/technogise/leave_management_system/service/LeaveService.java
+++ src/main/java/com/technogise/leave_management_system/service/LeaveService.java
@@ -1,4 +1,43 @@
 package com.technogise.leave_management_system.service;
 
+import com.technogise.leave_management_system.dto.LeaveResponse;
+import com.technogise.leave_management_system.entity.User;
+import com.technogise.leave_management_system.exception.ResourceNotFoundException;
+import com.technogise.leave_management_system.repository.LeaveRepository;
+import com.technogise.leave_management_system.repository.UserRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class LeaveService {
+    private final UserRepository userRepository;
+    private final LeaveRepository leaveRepository;
+    public LeaveService(UserRepository userRepository, LeaveRepository leaveRepository) {
+        this.userRepository = userRepository;
+        this.leaveRepository = leaveRepository;
+    }
+    public User findUserById(UUID id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("USER_NOT_FOUND", "User not found with id: " + id));
+    }
+    public List<LeaveResponse> getAllLeaves(UUID userId, String status, UUID requestedEmployeeId) {
+        User user = findUserById(userId);
+        List<LeaveResponse> leaveList= leaveRepository.findAllByUserId(userId, Sort.by(Sort.Direction.DESC, "createdAt"))
+                .stream()
+                .map(leave -> new LeaveResponse(
+                        leave.getId(),
+                        leave.getDate(),
+                        user.getName(),
+                        leave.getLeaveCategory().getName(),
+                        leave.getDuration(),
+                        leave.getStartTime(),
+                        leave.getUpdatedAt(),
+                        leave.getDescription()
+                )).toList();
+        return leaveList;
+    }
 }
