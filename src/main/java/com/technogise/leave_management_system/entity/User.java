2b295bdb38be4629c0b11cfc71c03ea965f5ae5d
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-03-24 17:43:56 +0530

    refactor: LMS-52 remove * imports from entities

--- src/main/java/com/technogise/leave_management_system/entity/User.java
+++ src/main/java/com/technogise/leave_management_system/entity/User.java
@@ -2,6 +2,8 @@ package com.technogise.leave_management_system.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
