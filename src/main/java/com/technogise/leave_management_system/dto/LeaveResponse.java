89198f4a5d07b45f1164f74f1c6be2ef202c4300
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-24 16:08:59 +0530

    feat(LMS-15): create a leave response dto

--- src/main/java/com/technogise/leave_management_system/dto/LeaveResponse.java
+++ src/main/java/com/technogise/leave_management_system/dto/LeaveResponse.java
@@ -1,10 +1,13 @@
 package com.technogise.leave_management_system.dto;
 
+import com.technogise.leave_management_system.enums.DurationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.UUID;
 
@@ -12,10 +15,13 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class LeaveListResponse {
+public class LeaveResponse {
     public UUID id;
     public Date date;
+    public String employeeName;
     public String type;
-    public String duration;
+    public DurationType duration;
+    public LocalTime startTime;
+    public LocalDateTime applyOn;
     public String reason;
 }
