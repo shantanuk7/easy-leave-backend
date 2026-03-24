580636b7e08de24022dd58bf4184691e7f8c5f8c
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-24 16:11:40 +0530

    feat(LMS-15):add GET endpoint for all leave retrieval with filters

--- src/main/java/com/technogise/leave_management_system/controller/LeaveController.java
+++ src/main/java/com/technogise/leave_management_system/controller/LeaveController.java
@@ -1,4 +1,29 @@
 package com.technogise.leave_management_system.controller;
 
+import com.technogise.leave_management_system.dto.LeaveResponse;
+import com.technogise.leave_management_system.response.SuccessResponse;
+import com.technogise.leave_management_system.service.LeaveService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/leaves")
 public class LeaveController {
+    private final LeaveService leaveService;
+    public LeaveController(LeaveService leaveService) {
+        this.leaveService = leaveService;
+    }
+    @GetMapping
+    public ResponseEntity<SuccessResponse<List<LeaveResponse>>> findAllLeave(
+            @RequestParam(name = "status", required = false) String status,
+            @RequestParam(name = "requested_employee_id",required = false)UUID requestedEmployeeId,
+            @RequestHeader(name = "user_id",required = false) UUID userId
+            ){
+        List<LeaveResponse> leaveList = leaveService.getAllLeaves(userId,status,requestedEmployeeId);
+        return ResponseEntity.status(HttpStatus.OK).body(SuccessResponse.success("Leaves retrieved successfully",leaveList));
+    }
 }
