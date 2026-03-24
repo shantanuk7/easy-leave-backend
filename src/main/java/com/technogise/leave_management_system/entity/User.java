71a6d8f997d55d131c493ee78bfaa12b643346e3
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-03-24 12:01:22 +0530

    refactor: LMS-52 keep only required columns for happy path for User

--- src/main/java/com/technogise/leave_management_system/entity/User.java
+++ src/main/java/com/technogise/leave_management_system/entity/User.java
@@ -1,10 +1,8 @@
 package com.technogise.leave_management_system.entity;
 
-import com.technogise.leave_management_system.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.UUID;
 
 @AllArgsConstructor @NoArgsConstructor @Getter @Setter
@@ -14,19 +12,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-
-    @Column(name = "name", nullable = false, length = 50)
-    private String name;
-
-    @Column(name = "email", nullable = false, unique = true)
-    private String email;
-
-    @Column(name = "role", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private UserRole role;
-
-    @Column(name = "joining_date", nullable = false)
-    private Date joiningDate;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
