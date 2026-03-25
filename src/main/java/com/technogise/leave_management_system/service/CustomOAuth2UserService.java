a3fb58eb7d70351f208d4fd74111f22b73909903
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-03-25 13:24:36 +0530

    refactor(LMS-5): seperate user creation in db logic

--- src/main/java/com/technogise/leave_management_system/service/CustomOAuth2UserService.java
+++ src/main/java/com/technogise/leave_management_system/service/CustomOAuth2UserService.java
@@ -1,9 +1,6 @@
 package com.technogise.leave_management_system.service;
 
-import com.technogise.leave_management_system.entity.User;
-import com.technogise.leave_management_system.enums.UserRole;
-import com.technogise.leave_management_system.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -12,28 +9,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserService userService;
+    private final String allowedDomain;
+
+    public CustomOAuth2UserService( UserService userService, @Value("${app.allowed-email-domain}") String allowedDomain)
+    {
+        this.userService = userService;
+        this.allowedDomain = allowedDomain;
+    }
+
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
         OAuth2User user = super.loadUser(userRequest);
         String email = user.getAttribute("email");
         String name = user.getAttribute("name");
 
-        if (email != null && !email.endsWith("@technogise.com")) {
+        if (!isValidEmail(email))
+        {
             throw new OAuth2AuthenticationException("Unauthorized email domain");
         }
-        System.out.println(email);
-        System.out.println(name);
-        User existingUser = userRepository.findByEmail(email);
-        if (existingUser == null) {
-            User newUser = new User();
-            newUser.setEmail(email);
-            newUser.setName(name);
-            newUser.setRole(UserRole.EMPLOYEE);
-            userRepository.save(newUser);
-        }
+
+        userService.findOrCreateUser(email, name);
+
         return user;
+    }
+
+    private boolean isValidEmail(String email) {
+        return email != null && email.endsWith(allowedDomain);
     }
 }
---
+++ src/main/java/com/technogise/leave_management_system/service/UserService.java
@@ -0,0 +1,31 @@
+package com.technogise.leave_management_system.service;
+
+import com.technogise.leave_management_system.entity.User;
+import com.technogise.leave_management_system.enums.UserRole;
+import com.technogise.leave_management_system.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void findOrCreateUser(String email, String name) {
+        userRepository.findByEmail(email)
+                .orElseGet(() -> createUser(email, name));
+    }
+
+    private User createUser(String email, String name) {
+        User newUser = new User();
+        newUser.setEmail(email);
+        newUser.setName(name);
+        newUser.setRole(UserRole.EMPLOYEE);
+        return userRepository.save(newUser);
+    }
+
+
+}
