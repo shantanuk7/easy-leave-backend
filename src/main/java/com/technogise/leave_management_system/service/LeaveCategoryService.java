49df4bc4b943bbf4060dc7ee54d5671fb590ddc0
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-03-25 18:21:58 +0530

    chore:LMS-8 add test for leavecategory not found exception

---
+++ src/main/java/com/technogise/leave_management_system/service/LeaveCategoryService.java
@@ -0,0 +1,23 @@
+package com.technogise.leave_management_system.service;
+
+import com.technogise.leave_management_system.entity.LeaveCategory;
+import com.technogise.leave_management_system.exception.NotFoundException;
+import com.technogise.leave_management_system.repository.LeaveCategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class LeaveCategoryService {
+
+    private final LeaveCategoryRepository leaveCategoryRepository;
+
+    public LeaveCategoryService(LeaveCategoryRepository leaveCategoryRepository) {
+        this.leaveCategoryRepository = leaveCategoryRepository;
+    }
+
+    public LeaveCategory findLeaveCategoryById(UUID leaveCategoryId) {
+        return leaveCategoryRepository.findById(leaveCategoryId)
+                .orElseThrow(() -> new NotFoundException("LeaveCategory not found"));
+    }
+}
