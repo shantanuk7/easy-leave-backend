b7769d1dab4942466a7a9b6e10036fcdf7b09c27
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-27 01:06:50 +0530

    refactor(LMS-15): update GlobalExceptionHandler to handle ApplicationException

--- src/main/java/com/technogise/leave_management_system/exception/GlobalExceptionHandler.java
+++ src/main/java/com/technogise/leave_management_system/exception/GlobalExceptionHandler.java
@@ -1,26 +1,20 @@
 package com.technogise.leave_management_system.exception;
 
 import com.technogise.leave_management_system.response.ErrorResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException exception) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).
-                body(new ErrorResponse("404",exception.getCode(), exception.getMessage()));
-    }
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).
-                body(new ErrorResponse("400",exception.getCode(), exception.getMessage()));
-    }
-    @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<ErrorResponse> handleAccessDeniedException(ForbiddenException exception) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).
-                body(new ErrorResponse("403",exception.getCode(), exception.getMessage()));
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<ErrorResponse> handleApplicationException(ApplicationException ex) {
+        return ResponseEntity.status(ex.getStatusCode())
+                .body(new ErrorResponse(
+                        String.valueOf(ex.getStatusCode().value()),
+                        ex.getCode(),
+                        ex.getMessage()
+                ));
     }
 }
