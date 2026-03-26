485c3571103554d505f0a1a825e33b021a84705b
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-27 01:22:31 +0530

    test(LMS-15): add unit test for GlobalExceptionHandler

---
+++ src/test/java/com/technogise/leave_management_system/exception/GlobalExceptionHandlerTest.java
@@ -0,0 +1,31 @@
+package com.technogise.leave_management_system.exception;
+
+
+import com.technogise.leave_management_system.response.ErrorResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class GlobalExceptionHandlerTest {
+
+    private final GlobalExceptionHandler handler = new GlobalExceptionHandler();
+
+    @Test
+    void shouldReturnProperErrorResponseWhenApplicationExceptionThrown() {
+        ApplicationException exception = new ApplicationException(
+                HttpStatus.NOT_FOUND,
+                "NOT_FOUND",
+                "User not found"
+        );
+        ResponseEntity<ErrorResponse> response =
+                handler.handleApplicationException(exception);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("404", response.getBody().getStatusCode());
+        assertEquals("NOT_FOUND", response.getBody().getCode());
+        assertEquals("User not found", response.getBody().getMessage());
+    }
+}
