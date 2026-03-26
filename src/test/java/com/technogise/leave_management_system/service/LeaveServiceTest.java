7e8a5374d90f11304bae1d1bdb75e420395d7b70
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-27 00:03:12 +0530

    test(LMS-15): setup LeaveService test configuration and test data

---
+++ src/test/java/com/technogise/leave_management_system/service/LeaveServiceTest.java
@@ -0,0 +1,62 @@
+package com.technogise.leave_management_system.service;
+
+import com.technogise.leave_management_system.entity.Leave;
+import com.technogise.leave_management_system.entity.LeaveCategory;
+import com.technogise.leave_management_system.entity.User;
+import com.technogise.leave_management_system.enums.DurationType;
+import com.technogise.leave_management_system.enums.UserRole;
+import com.technogise.leave_management_system.repository.LeaveRepository;
+import com.technogise.leave_management_system.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+public class LeaveServiceTest {
+    @Mock
+    private LeaveRepository leaveRepository;
+
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private LeaveService leaveService;
+
+    User employee = new User();
+    User manager = new User();
+    LeaveCategory leaveCategory = new LeaveCategory();
+    Leave employeeLeave = new Leave();
+    Leave managerLeave = new Leave();
+
+    @BeforeEach
+    public void setUp() {
+        employee.setId(UUID.randomUUID());
+        employee.setName("Employee");
+        employee.setRole(UserRole.EMPLOYEE);
+
+        manager.setId(UUID.randomUUID());
+        manager.setName("Manager");
+        manager.setRole(UserRole.MANAGER);
+
+        leaveCategory.setId(UUID.randomUUID());
+        leaveCategory.setName("Annual Leave");
+        leaveCategory.setCreatedAt(LocalDateTime.now());
+        leaveCategory.setUpdatedAt(LocalDateTime.now());
+
+        employeeLeave.setId(UUID.randomUUID());
+        employeeLeave.setUser(employee);
+        employeeLeave.setLeaveCategory(leaveCategory);
+        employeeLeave.setDate(LocalDate.of(2026, 3, 30));
+        employeeLeave.setDuration(DurationType.FULL_DAY);
+        employeeLeave.setDescription("Personal Work");
+        employeeLeave.setStartTime(LocalTime.now());
+        employeeLeave.setCreatedAt(LocalDateTime.now());
+        employeeLeave.setUpdatedAt(LocalDateTime.now());
+    }
+}
