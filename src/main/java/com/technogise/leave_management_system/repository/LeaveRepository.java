b31367f907d94778e235039f4efbb69efaa77343
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-27 17:35:22 +0530

    fix(LMS-15): resolve N+1 query issue in LeaveService using @EntityGraph

--- src/main/java/com/technogise/leave_management_system/repository/LeaveRepository.java
+++ src/main/java/com/technogise/leave_management_system/repository/LeaveRepository.java
@@ -2,6 +2,7 @@ package com.technogise.leave_management_system.repository;
 
 import com.technogise.leave_management_system.entity.Leave;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 
 @Repository
 public interface LeaveRepository extends JpaRepository<Leave, UUID> {
+    @EntityGraph(attributePaths = {"user","leaveCategory"})
     List<Leave> findAllByUserId(UUID userId, Sort sort);
 }
 
