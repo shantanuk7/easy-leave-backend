b5a83c916d0076103076c2b816e457e6d1dc96ac
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-03-30 15:57:21 +0530

    feat(LMS-5): integrate OAuth2success handler to handle login success response

---
+++ src/main/java/com/technogise/leave_management_system/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,44 @@
+package com.technogise.leave_management_system.handler;
+
+import com.technogise.leave_management_system.entity.User;
+import com.technogise.leave_management_system.repository.UserRepository;
+import com.technogise.leave_management_system.response.SuccessResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+
+    OAuth2LoginSuccessHandler(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public void onAuthenticationSuccess(@NonNull HttpServletRequest request,
+                                        @NonNull HttpServletResponse response,
+                                        @NonNull Authentication authentication) throws IOException {
+
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String email = oAuth2User.getAttribute("email");
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        SuccessResponse<User> successResponse = SuccessResponse.success("Login successful", user);
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+        objectMapper.writeValue(response.getWriter(), successResponse);
+    }
+}
