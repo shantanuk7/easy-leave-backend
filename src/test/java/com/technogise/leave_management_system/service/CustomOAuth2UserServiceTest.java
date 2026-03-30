a8e51b0dedf185166d5707f82f6c414e92aff5fe
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-03-30 13:33:30 +0530

    refactor(LMS-4):replace Mockito spy with WireMock

--- src/test/java/com/technogise/leave_management_system/service/CustomOAuth2UserServiceTest.java
+++ src/test/java/com/technogise/leave_management_system/service/CustomOAuth2UserServiceTest.java
@@ -5,7 +5,6 @@ import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -16,58 +15,21 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import java.time.Instant;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 import static org.springframework.security.oauth2.core.OAuth2AccessToken.TokenType.BEARER;
 
 @ExtendWith(MockitoExtension.class)
 @WireMockTest
 public class CustomOAuth2UserServiceTest {
 
-    @Mock
-    private OAuth2UserRequest userRequest;
-
-    @Mock
-    private OAuth2User oAuth2User;
-
     private CustomOAuth2UserService customOAuth2UserService;
-
     private static final String ALLOWED_DOMAIN = "@technogise.com";
 
     @BeforeEach
     void setUp() {
-        customOAuth2UserService = spy(new CustomOAuth2UserService(ALLOWED_DOMAIN));
+        customOAuth2UserService = new CustomOAuth2UserService(ALLOWED_DOMAIN);
     }
 
-    @Test
-    void shouldThrowExceptionWhenEmailIsNull() {
-        doReturn(oAuth2User).when(customOAuth2UserService).fetchOAuth2User(userRequest);
-        when(oAuth2User.getAttribute("email")).thenReturn(null);
-
-        assertThrows(
-                OAuth2AuthenticationException.class,
-                () -> customOAuth2UserService.loadUser(userRequest)
-        );
-    }
-
-    @Test
-    void shouldThrowExceptionWhenEmailIsEmptyString() {
-        doReturn(oAuth2User).when(customOAuth2UserService).fetchOAuth2User(userRequest);
-        when(oAuth2User.getAttribute("email")).thenReturn("");
-
-        assertThrows(OAuth2AuthenticationException.class, () -> customOAuth2UserService.loadUser(userRequest));
-    }
-
-    @Test
-    void shouldFetchOAuth2UserWithMockedOAuthServer(WireMockRuntimeInfo wmRuntimeInfo) {
-        stubFor(get("/userinfo")
-                .willReturn(okJson("""
-                        {
-                          "sub": "12345",
-                          "email": "rakshit@technogise.com",
-                          "name": "Rakshit Saxena"
-                        }
-                        """)));
-
+    private OAuth2UserRequest buildRequest(WireMockRuntimeInfo wm) {
         ClientRegistration clientRegistration = ClientRegistration
                 .withRegistrationId("google")
                 .clientId("fake-client-id")
@@ -76,16 +38,50 @@ public class CustomOAuth2UserServiceTest {
                 .redirectUri("http://localhost/login/oauth2/code/google")
                 .authorizationUri("http://localhost/oauth2/authorize")
                 .tokenUri("http://localhost/oauth2/token")
-                .userInfoUri(wmRuntimeInfo.getHttpBaseUrl() + "/userinfo")
+                .userInfoUri(wm.getHttpBaseUrl() + "/userinfo")
                 .userNameAttributeName("sub")
                 .build();
 
-        OAuth2UserRequest wireMockUserRequest = new OAuth2UserRequest(clientRegistration,
-                new OAuth2AccessToken(BEARER, "fake-token", Instant.now(), Instant.now().plusSeconds(3600)));
+        return new OAuth2UserRequest(clientRegistration,
+                new OAuth2AccessToken(BEARER, "fake-token",
+                        Instant.now(), Instant.now().plusSeconds(3600)));
+    }
 
-        CustomOAuth2UserService service = new CustomOAuth2UserService(ALLOWED_DOMAIN);
-        OAuth2User result = service.loadUser(wireMockUserRequest);
+
+    @Test
+    void shouldThrowExceptionWhenEmailIsEmptyString(WireMockRuntimeInfo wm) {
+        stubFor(get("/userinfo").willReturn(okJson("""
+                { "sub": "01", "email": "", "name": "Rakshit" }
+                """)));
+
+        assertThrows(OAuth2AuthenticationException.class,
+                () -> customOAuth2UserService.loadUser(buildRequest(wm)));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailDomainIsNotAllowed(WireMockRuntimeInfo wm) {
+        stubFor(get("/userinfo").willReturn(okJson("""
+                { "sub": "01", "email": "rakshit@gmail.com", "name": "Rakshit" }
+                """)));
+
+        assertThrows(OAuth2AuthenticationException.class, () -> customOAuth2UserService.loadUser(buildRequest(wm)));
+    }
+
+    @Test
+    void shouldReturnUserWhenEmailDomainIsValid(WireMockRuntimeInfo wm) {
+        stubFor(get("/userinfo").willReturn(okJson("""
+                { "sub": "123", "email": "rakshit@technogise.com", "name": "Rakshit Saxena" }""")));
+
+        OAuth2User result = customOAuth2UserService.loadUser(buildRequest(wm));
 
         assertEquals("rakshit@technogise.com", result.getAttribute("email"));
+    }
+    @Test
+    void shouldThrowExceptionWhenEmailIsNull(WireMockRuntimeInfo wm) {
+        stubFor(get("/userinfo").willReturn(okJson("""
+                 { "sub": "01", "name": "Rakshit" }""")));
+
+        assertThrows(OAuth2AuthenticationException.class,
+                () -> customOAuth2UserService.loadUser(buildRequest(wm)));
     }
 }
