1b082746c1c22d80b1b0f93c4997f5d0e2cddd9a
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-02 13:36:07 +0530

    test(LMS-19):add initial UserControllerTest with setup

---
+++ src/test/java/com/technogise/leave_management_system/controller/UserControllerTest.java
@@ -0,0 +1,52 @@
+package com.technogise.leave_management_system.controller;
+
+import com.technogise.leave_management_system.dto.UserResponse;
+import com.technogise.leave_management_system.entity.User;
+import com.technogise.leave_management_system.enums.UserRole;
+import com.technogise.leave_management_system.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @MockitoBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private User employee;
+    private User admin;
+
+    @BeforeEach
+    void setUp() {
+        employee = new User();
+        employee.setId(UUID.randomUUID());
+        employee.setName("Priyansh Saxena");
+        employee.setRole(UserRole.EMPLOYEE);
+        employee.setEmail("priyansh@gmail.com");
+
+        admin = new User();
+        admin.setId(UUID.randomUUID());
+        admin.setName("ADMIN");
+        admin.setRole(UserRole.ADMIN);
+        admin.setEmail("admin@gmail.com");
+    }
+}
