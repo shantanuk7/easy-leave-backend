fc8db7a57a00630db17363731755555bb57aa4ec
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-02 13:45:38 +0530

    fix(LMS-19):remove un-used import and add new line at end of file

--- src/test/java/com/technogise/leave_management_system/service/UserServiceTest.java
+++ src/test/java/com/technogise/leave_management_system/service/UserServiceTest.java
@@ -12,8 +12,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
