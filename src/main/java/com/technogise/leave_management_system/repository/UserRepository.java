315a5b366d0b006536782dcb2841e043d18c540b
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-03 18:50:05 +0530

    feat(LMS-19): add pageable method to fetch users ordered by name

--- src/main/java/com/technogise/leave_management_system/repository/UserRepository.java
+++ src/main/java/com/technogise/leave_management_system/repository/UserRepository.java
@@ -1,15 +1,17 @@
 package com.technogise.leave_management_system.repository;
 
 import com.technogise.leave_management_system.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
-    List<User> findByOrderByNameAsc();
+    Page<User> findAllByOrderByNameAsc(Pageable pageable);
 }
+
