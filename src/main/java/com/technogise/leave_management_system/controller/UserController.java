1708f998fedc22b38bc4f85ef8bc6e05c63fbc21
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-03 15:08:32 +0530

    fix(LMS-19): replace RequestHeader with AuthenticationPrincipal to follow jwt auth

--- src/main/java/com/technogise/leave_management_system/controller/UserController.java
+++ src/main/java/com/technogise/leave_management_system/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.technogise.leave_management_system.controller;
 import com.technogise.leave_management_system.dto.UserResponse;
+import com.technogise.leave_management_system.entity.User;
 import com.technogise.leave_management_system.response.SuccessResponse;
 import com.technogise.leave_management_system.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +23,9 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<SuccessResponse<List<UserResponse>>> getAllUsers(
-            @RequestHeader(name = "user_id") UUID userId
+            @AuthenticationPrincipal User user
     ) {
+        UUID userId = user.getId();
         List<UserResponse> usersList = userService.getAllUsers(userId);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(SuccessResponse.success("Users retrieved successfully", usersList));
