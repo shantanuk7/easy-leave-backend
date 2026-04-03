56e8b794b9c37f09841992e808b23bff44a192f9
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-03 18:51:09 +0530

    feat(lms-19): implement paginated user retrieval in UserService

--- src/main/java/com/technogise/leave_management_system/service/UserService.java
+++ src/main/java/com/technogise/leave_management_system/service/UserService.java
@@ -5,10 +5,11 @@ import com.technogise.leave_management_system.entity.User;
 import com.technogise.leave_management_system.enums.UserRole;
 import com.technogise.leave_management_system.exception.HttpException;
 import com.technogise.leave_management_system.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -37,15 +38,15 @@ public class UserService {
         newUser.setRole(UserRole.EMPLOYEE);
         return userRepository.save(newUser);
     }
-    public List<UserResponse> getAllUsers() {
-        List<User> allUsers = userRepository.findByOrderByNameAsc();
-        return allUsers.stream()
+    public Page<UserResponse> getAllUsers(Pageable pageable) {
+        return userRepository.findAllByOrderByNameAsc(pageable)
                 .map(u -> new UserResponse(
                         u.getId(),
                         u.getEmail(),
                         u.getName(),
                         u.getRole()
-                )).toList();
+                ));
     }
 }
+
 
