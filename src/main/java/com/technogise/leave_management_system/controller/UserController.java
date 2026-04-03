8d13c0051801f1c52b6357b37ff0017a09d9759b
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-03 18:53:05 +0530

    refactor(lms-19):update getAllUsers endpoint to fetch users using Pageable

--- src/main/java/com/technogise/leave_management_system/controller/UserController.java
+++ src/main/java/com/technogise/leave_management_system/controller/UserController.java
@@ -1,19 +1,17 @@
 package com.technogise.leave_management_system.controller;
 import com.technogise.leave_management_system.dto.UserResponse;
-import com.technogise.leave_management_system.entity.User;
 import com.technogise.leave_management_system.response.SuccessResponse;
 import com.technogise.leave_management_system.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
@@ -25,9 +23,11 @@ public class UserController {
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER')")
-    public ResponseEntity<SuccessResponse<List<UserResponse>>> getAllUsers() {
-        List<UserResponse> usersList = userService.getAllUsers();
+    public ResponseEntity<SuccessResponse<List<UserResponse>>> getAllUsers(
+            Pageable pageable
+    ) {
+        Page<UserResponse> usersPage = userService.getAllUsers(pageable);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(SuccessResponse.success("Users retrieved successfully", usersList));
+                .body(SuccessResponse.success("Users retrieved successfully", usersPage.getContent()));
     }
 }
