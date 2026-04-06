e2437142f8704dd8fe0d98892f3418efd0cfeef6
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-06 13:08:34 +0530

    refactor(LMS-62): reuse find all already presented query method

--- src/main/java/com/technogise/leave_management_system/repository/UserRepository.java
+++ src/main/java/com/technogise/leave_management_system/repository/UserRepository.java
@@ -3,11 +3,9 @@ package com.technogise.leave_management_system.repository;
 import com.technogise.leave_management_system.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import com.technogise.leave_management_system.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,5 +13,4 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     Page<User> findAllByOrderByNameAsc(Pageable pageable);
-    List<User> findAllByRole(UserRole role);
 }
