bbbceecf932362cb6b65cb991011ada460889415
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-06 17:33:28 +0530

    feat(LMS-62): add query method to fetch distinct years

--- src/main/java/com/technogise/leave_management_system/repository/AnnualLeaveRepository.java
+++ src/main/java/com/technogise/leave_management_system/repository/AnnualLeaveRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -16,4 +18,7 @@ public interface AnnualLeaveRepository extends JpaRepository<AnnualLeave, UUID> 
     @EntityGraph(attributePaths = {"user"})
     @Query("SELECT al FROM annual_leaves al WHERE al.year = :year")
     Page<AnnualLeave> findAllByYear(@Param("year") String year, Pageable pageable);
+
+    @Query("SELECT DISTINCT al.year FROM annual_leaves al ORDER BY al.year DESC")
+    List<String> findDistinctYears();
 }
