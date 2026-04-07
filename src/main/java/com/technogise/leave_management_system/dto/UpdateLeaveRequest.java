a5506575709f745df8d0bf830f904ad76a16a25a
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-04-07 17:25:54 +0530

    refactor: LMS-23 remove annotations from dto

--- src/main/java/com/technogise/leave_management_system/dto/UpdateLeaveRequest.java
+++ src/main/java/com/technogise/leave_management_system/dto/UpdateLeaveRequest.java
@@ -1,8 +1,6 @@
 package com.technogise.leave_management_system.dto;
 
 import com.technogise.leave_management_system.enums.DurationType;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,19 +17,14 @@ import java.util.UUID;
 @NoArgsConstructor
 public class UpdateLeaveRequest {
 
-    @NotNull(message = "Date must not be null")
     private LocalDate date;
 
-    @NotNull(message = "Start time must not be null")
     private LocalTime startTime;
 
-    @NotBlank(message = "Description must not be blank")
     @Size(max = 1000, message = "Description must not exceed 1000 characters")
     private String description;
 
-    @NotNull(message = "Duration must not be null")
     private DurationType duration;
 
-    @NotNull(message = "leaveCategoryId must not be null")
     private UUID leaveCategoryId;
 }
