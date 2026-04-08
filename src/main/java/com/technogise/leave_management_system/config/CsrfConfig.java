3f2dd088b4147a464e8002125c562cf5bfc7b91c
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-04-08 15:36:01 +0530

    chore: implement the CsrfConfig file

---
+++ src/main/java/com/technogise/leave_management_system/config/CsrfConfig.java
@@ -0,0 +1,15 @@
+package com.technogise.leave_management_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+
+@Configuration
+public class CsrfConfig {
+
+    @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+        return CookieCsrfTokenRepository.withHttpOnlyFalse();
+    }
+}
