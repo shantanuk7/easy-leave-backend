c812dff38bbbd26399ec6017a644aad201c00b2c
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-09 12:11:50 +0530

    feat(lms-72): add DashboardService to fetch employee metrics for manager dashboard

---
+++ src/main/java/com/technogise/leave_management_system/service/DashboardService.java
@@ -0,0 +1,31 @@
+
+package com.technogise.leave_management_system.service;
+
+import com.technogise.leave_management_system.dto.EmployeeMetricsResponse;
+import com.technogise.leave_management_system.repository.LeaveRepository;
+import com.technogise.leave_management_system.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class DashboardService {
+    private final UserRepository userRepository;
+    private final LeaveRepository leaveRepository;
+
+    public DashboardService(UserRepository userRepository, LeaveRepository leaveRepository) {
+        this.userRepository = userRepository;
+        this.leaveRepository = leaveRepository;
+    }
+
+    public EmployeeMetricsResponse getManagerDashboardData() {
+        LocalDate today = LocalDate.now();
+        LocalDate tomorrow = today.plusDays(1);
+
+        long totalEmployees = userRepository.count();
+        long onLeaveToday = leaveRepository.countByDate(today);
+        long onLeaveTomorrow = leaveRepository.countByDate(tomorrow);
+        return new EmployeeMetricsResponse(totalEmployees, onLeaveToday, onLeaveTomorrow);
+    }
+}
+
