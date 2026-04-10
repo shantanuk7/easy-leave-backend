c4b71d9415434fb5ddc4002e8d64058f15dd7642
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-10 13:04:35 +0530

    feat(LMS-67): add sync  method call on leaves getting created and updated

--- src/main/java/com/technogise/leave_management_system/service/LeaveService.java
+++ src/main/java/com/technogise/leave_management_system/service/LeaveService.java
@@ -8,6 +8,7 @@ import com.technogise.leave_management_system.dto.UpdateLeaveResponse;
 import com.technogise.leave_management_system.entity.Leave;
 import com.technogise.leave_management_system.entity.LeaveCategory;
 import com.technogise.leave_management_system.entity.User;
+import com.technogise.leave_management_system.enums.DurationType;
 import com.technogise.leave_management_system.enums.UserRole;
 import com.technogise.leave_management_system.enums.WeekendDay;
 import com.technogise.leave_management_system.exception.HttpException;
@@ -39,13 +40,15 @@ public class LeaveService {
     private final LeaveRepository leaveRepository;
     private final UserService userService;
     private final LeaveCategoryService leaveCategoryService;
+    private final AnnualLeaveService annualLeaveService;
 
     public LeaveService(LeaveRepository leaveRepository,
                         UserService userService,
-                        LeaveCategoryService leaveCategoryService) {
+                        LeaveCategoryService leaveCategoryService, AnnualLeaveService annualLeaveService) {
         this.leaveRepository = leaveRepository;
         this.userService = userService;
         this.leaveCategoryService = leaveCategoryService;
+        this.annualLeaveService = annualLeaveService;
     }
 
     public List<Leave> filterLeavesByScope(String scope, User user) {
@@ -189,8 +192,8 @@ public class LeaveService {
                     leave.setDuration(request.getDuration());
                     return leave;
                 }).toList();
-
         List<Leave> savedLeaves = leaveRepository.saveAll(leavesToSave);
+        annualLeaveService.syncOnLeaveCreated(user, request.getDuration(), newDatesToApply.size(), LocalDate.now().getYear());
         return savedLeaves.stream()
                 .map(leave -> new CreateLeaveResponse(
                         leave.getId(),
@@ -211,11 +214,7 @@ public class LeaveService {
             throw new HttpException(HttpStatus.FORBIDDEN, "Not Allowed to access this resource");
         }
 
-        return new LeaveResponse(
-                leave.getId(),
-                leave.getDate(),
-                leave.getUser().getName(),
-                leave.getLeaveCategory().getName(),
+        return new LeaveResponse(leave.getId(), leave.getDate(), leave.getUser().getName(), leave.getLeaveCategory().getName(),
                 leave.getDuration(),
                 leave.getStartTime(),
                 leave.getUpdatedAt(),
@@ -236,6 +235,8 @@ public class LeaveService {
         validateLeaveOwnership(leave, userId);
         validateExistingLeaveDate(leave.getDate());
 
+        DurationType oldDuration = leave.getDuration();     //full day or half day
+
         if (request.getDate() != null) {
             validateNewLeaveDate(request.getDate());
             validateNewLeaveDateIsNotWeekend(request.getDate());
@@ -251,6 +252,11 @@ public class LeaveService {
         Optional.ofNullable(request.getDescription()).ifPresent(leave::setDescription);
 
         Leave savedLeave = leaveRepository.save(leave);
+
+        if (request.getDuration() != null) {
+            annualLeaveService.syncOnLeaveUpdated(leave.getUser(), oldDuration, request.getDuration(), leave.getDate().getYear());
+        }
+
         return mapToUpdateLeaveResponse(savedLeave);
     }
 
