c425cc9591ffe0078cc8d6b59c2f3d133337c2ab
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-04-10 14:18:07 +0530

    fix:LMS-73 add samesite and secure from env in CSRFToken

--- src/main/java/com/technogise/leave_management_system/config/CsrfConfig.java
+++ src/main/java/com/technogise/leave_management_system/config/CsrfConfig.java
@@ -1,5 +1,6 @@
 package com.technogise.leave_management_system.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -8,8 +9,22 @@ import org.springframework.security.web.csrf.CsrfTokenRepository;
 @Configuration
 public class CsrfConfig {
 
+    @Value("${app.cookie.secure}")
+    private boolean cookieSecure;
+
+    @Value("${app.cookie.same.site}")
+    private String cookieSameSite;
+
     @Bean
     public CsrfTokenRepository csrfTokenRepository() {
-        return CookieCsrfTokenRepository.withHttpOnlyFalse();
+        CookieCsrfTokenRepository repository =
+                CookieCsrfTokenRepository.withHttpOnlyFalse();
+        repository.setCookieCustomizer(cookie -> {
+            cookie.sameSite(cookieSameSite);
+            cookie.secure(cookieSecure);
+            cookie.path("/");
+        });
+
+        return repository;
     }
 }
