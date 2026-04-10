b488bb6a8de3317c17346f2c77a3f141e6b9e4ff
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-10 12:50:56 +0530

    feat(LMS-67): add  method to find leave category

--- src/main/java/com/technogise/leave_management_system/repository/LeaveCategoryRepository.java
+++ src/main/java/com/technogise/leave_management_system/repository/LeaveCategoryRepository.java
@@ -4,8 +4,10 @@ import com.technogise.leave_management_system.entity.LeaveCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface LeaveCategoryRepository extends JpaRepository<LeaveCategory, UUID> {
+    Optional<LeaveCategory> findByName(String name);
 }
