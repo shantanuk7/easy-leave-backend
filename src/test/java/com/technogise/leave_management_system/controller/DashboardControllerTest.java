d7eae348611b46ddd7c1e64efa5757831dc3e271
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-13 11:38:14 +0530

    fix(lms-72): fix the test case of dashboard controller to use MethodSecurity for role base access

--- src/test/java/com/technogise/leave_management_system/controller/DashboardControllerTest.java
+++ src/test/java/com/technogise/leave_management_system/controller/DashboardControllerTest.java
@@ -1,38 +1,31 @@
 package com.technogise.leave_management_system.controller;
 
 import com.technogise.leave_management_system.dto.EmployeeMetricsResponse;
-import com.technogise.leave_management_system.entity.User;
-import com.technogise.leave_management_system.enums.UserRole;
 import com.technogise.leave_management_system.repository.LeaveRepository;
 import com.technogise.leave_management_system.repository.UserRepository;
 import com.technogise.leave_management_system.service.DashboardService;
 import com.technogise.leave_management_system.service.JwtService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.security.oauth2.client.autoconfigure.OAuth2ClientAutoConfiguration;
 import org.springframework.boot.security.oauth2.client.autoconfigure.servlet.OAuth2ClientWebSecurityAutoConfiguration;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
-
-import java.util.List;
-import java.util.UUID;
-
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc
 @WebMvcTest(value = DashboardController.class, excludeAutoConfiguration = {
     OAuth2ClientAutoConfiguration.class,
     OAuth2ClientWebSecurityAutoConfiguration.class
 })
+@EnableMethodSecurity(prePostEnabled = true)
 public class DashboardControllerTest {
     @MockitoBean
     private DashboardService dashboardService;
@@ -49,37 +42,25 @@ public class DashboardControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private User manager;
-
-    private RequestPostProcessor mockUser(User user) {
-        return request -> {
-            UsernamePasswordAuthenticationToken auth =
-                    new UsernamePasswordAuthenticationToken(user, null, List.of());
-            SecurityContextHolder.getContext().setAuthentication(auth);
-            request.setUserPrincipal(auth);
-            return request;
-        };
-    }
-
-    @BeforeEach
-    void setUp() {
-        manager = new User();
-        manager.setId(UUID.randomUUID());
-        manager.setName("Priyansh Saxena");
-        manager.setRole(UserRole.EMPLOYEE);
-        manager.setEmail("priyansh@technogise.com");
-    }
-
     @Test
+    @WithMockUser(roles = "MANAGER")
     void shouldReturn200WithListOfAllDetailsForManagerDashboard() throws Exception {
         EmployeeMetricsResponse response = new EmployeeMetricsResponse(20,20,20);
         when(dashboardService.getManagerDashboardData()).thenReturn(response);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/dashboard/manager")
-                        .with(mockUser(manager)))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/dashboard/manager"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("Manager Dashboard data retrieved successfully"))
                 .andExpect(jsonPath("$.data.totalEmployees").value(20));
+    }
+    @Test
+    @WithMockUser(roles = "EMPLOYEE")
+    void shouldReturn500WhenEmployeeRequestsManagerDashboard() throws Exception {
+        EmployeeMetricsResponse response = new EmployeeMetricsResponse(20,20,20);
+        when(dashboardService.getManagerDashboardData()).thenReturn(response);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/dashboard/manager"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value("Access Denied"));
     }
 }
 
