f1a693020b2c62423f916ff3e3a3939c9565cc48
RajDeshmukh2001 <rajdd61@gmail.com>
2026-04-16 16:21:46 +0530

    feat(LMS-18): add Holiday entity with HolidayType enum

---
+++ src/main/java/com/technogise/leave_management_system/entity/Holiday.java
@@ -0,0 +1,54 @@
+package com.technogise.leave_management_system.entity;
+
+import com.technogise.leave_management_system.enums.HolidayType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "holidays")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Holiday {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private HolidayType type;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
