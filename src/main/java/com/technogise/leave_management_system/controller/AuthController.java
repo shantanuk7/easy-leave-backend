12844bc70cdbbcbaf1664c0227ec6549e3bd42a0
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-19 14:25:46 +0530

    feat(LMS-69): add logout controller

--- src/main/java/com/technogise/leave_management_system/controller/AuthController.java
+++ src/main/java/com/technogise/leave_management_system/controller/AuthController.java
@@ -4,11 +4,15 @@ import com.technogise.leave_management_system.dto.AuthUserResponse;
 import com.technogise.leave_management_system.entity.User;
 import com.technogise.leave_management_system.response.SuccessResponse;
 import com.technogise.leave_management_system.service.AuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +23,10 @@ import java.util.UUID;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    @Value("${app.cookie.secure}")
+    private boolean cookieSecure;
+    @Value("${app.cookie.same.site}")
+    private String cookieSameSite;
 
     public AuthController(AuthService authService) {
         this.authService = authService;
@@ -37,5 +45,23 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(SuccessResponse.success("User retrieved successfully", authUser));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<SuccessResponse<Void>> logout(HttpServletResponse response) {
+        log.info("POST /api/auth/logout called");
+
+        Cookie cookie = new Cookie("token", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(cookieSecure);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        cookie.setAttribute("SameSite", cookieSameSite);
+
+        response.addCookie(cookie);
+
+        log.info("JWT cookie cleared successfully");
+
+        return ResponseEntity.ok(SuccessResponse.success("Logout successful", null));
     }
 }
