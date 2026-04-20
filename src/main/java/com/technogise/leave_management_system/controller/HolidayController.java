44df4bb0c852fd1e1fa9c7c721f8374df78fa0eb
shantanuk7 <shantanukulkarni365@gmail.com>
2026-04-20 12:34:15 +0530

    feat(LMS-30): accept holiday type in GET Holidays and throw exception if invalid, with controller and service tests
    # Conflicts:
    #	src/test/java/com/technogise/leave_management_system/controller/HolidayControllerTest.java

--- src/main/java/com/technogise/leave_management_system/controller/HolidayController.java
+++ src/main/java/com/technogise/leave_management_system/controller/HolidayController.java
@@ -12,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -40,8 +41,10 @@ public class HolidayController {
     }
 
     @GetMapping
-    public ResponseEntity<SuccessResponse<List<HolidayResponse>>> getHolidays() {
-        List<HolidayResponse> response = holidayService.getHolidays();
+    public ResponseEntity<SuccessResponse<List<HolidayResponse>>> getHolidays(
+            @RequestParam(name = "type", required = false) String type
+    ) {
+        List<HolidayResponse> response = holidayService.getHolidays(type);
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(SuccessResponse.success("Holidays retrieved successfully", response));
