eaa6e8fba4bbcaeaf6f9fc97f85405b1136377f6
RajDeshmukh2001 <rajdd61@gmail.com>
2026-04-21 10:12:27 +0530

    feat(LMS-78): add service method to get user details by id

--- src/main/java/com/technogise/leave_management_system/dto/UserResponse.java
+++ src/main/java/com/technogise/leave_management_system/dto/UserResponse.java
@@ -1,10 +1,13 @@
 package com.technogise.leave_management_system.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.technogise.leave_management_system.enums.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
 @Getter
 @Setter
@@ -13,5 +16,10 @@ public class UserResponse {
     private String email;
     private String name;
     private UserRole role;
+
+    public UserResponse(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
 }
 
