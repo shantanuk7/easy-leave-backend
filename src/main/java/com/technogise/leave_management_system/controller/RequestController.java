24d6022c5143a624a4caaae8ab85661cc75b4a55
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-23 01:50:14 +0530

    feat(lms-36):implement GET /api/requests with status filters

---
+++ src/main/java/com/technogise/leave_management_system/controller/RequestController.java
@@ -0,0 +1,38 @@
+package com.technogise.leave_management_system.controller;
+
+import com.technogise.leave_management_system.dto.GetAllRequestResponse;
+import com.technogise.leave_management_system.entity.User;
+import com.technogise.leave_management_system.enums.RequestStatus;
+import com.technogise.leave_management_system.enums.ScopeType;
+import com.technogise.leave_management_system.response.SuccessResponse;
+import com.technogise.leave_management_system.service.RequestService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/requests")
+public class RequestController {
+    private final RequestService requestService;
+    public  RequestController(RequestService requestService) {
+        this.requestService = requestService;
+    }
+    @GetMapping
+    public ResponseEntity<SuccessResponse<Page<GetAllRequestResponse>>> getAllRequests(
+            Pageable pageable,
+            @RequestParam(defaultValue = "SELF") ScopeType scope,
+            @RequestParam(required = false) RequestStatus status,
+            @AuthenticationPrincipal User user
+    ) {
+        Page<GetAllRequestResponse> response =
+                requestService.getAllRequests(pageable, user.getId(), scope, status);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                SuccessResponse.success("Requests fetch Successfully", response));
+    }
+}
