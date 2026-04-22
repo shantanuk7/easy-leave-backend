082c948b52c4b52c2312061e9975353c7f88f5d4
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-23 01:44:20 +0530

    feat(lms-36): feat(service): add RequestService to fetch paginated leave requests for self scope

--- src/main/java/com/technogise/leave_management_system/repository/RequestRepository.java
+++ src/main/java/com/technogise/leave_management_system/repository/RequestRepository.java
@@ -1,10 +1,18 @@
 package com.technogise.leave_management_system.repository;
 
 import com.technogise.leave_management_system.entity.Request;
+import com.technogise.leave_management_system.enums.RequestStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RequestRepository extends JpaRepository<Request,Integer> {
+import java.util.UUID;
+public interface RequestRepository extends JpaRepository<Request, UUID> {
+
+    @EntityGraph(attributePaths = {"requestedByUser", "leaveCategory"})
+    Page<Request> findAllByRequestedByUserId(UUID userId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"requestedByUser", "leaveCategory"})
+    Page<Request> findAllByRequestedByUserIdAndStatus(UUID userId, RequestStatus status, Pageable pageable);
 }
-
