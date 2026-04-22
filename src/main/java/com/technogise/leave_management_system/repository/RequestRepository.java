4c1a938faa8fcaea2b0348eaae5f95bbf7fef3c4
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-04-22 17:07:50 +0530

    chore:LMS-35 add existsByRequestedByUserIdAndDateAndStatusIn method in RequestRepository

--- src/main/java/com/technogise/leave_management_system/repository/RequestRepository.java
+++ src/main/java/com/technogise/leave_management_system/repository/RequestRepository.java
@@ -1,12 +1,17 @@
 package com.technogise.leave_management_system.repository;
 
 import com.technogise.leave_management_system.entity.Request;
+import com.technogise.leave_management_system.enums.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, UUID> {
+    boolean existsByRequestedByUserIdAndDateAndStatusIn(
+            UUID requestedByUserId, LocalDate date, List<RequestStatus> statuses);
 }
