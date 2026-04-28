fb8d1f081a40e81acd80aee7e6e953d501f80818
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-29 00:57:32 +0530

    feat(LMS-79): add findByLeaveIdAndPlatformAndDeletedAtIsNull query method

--- src/main/java/com/technogise/leave_management_system/repository/LeaveIntegrationEventRepository.java
+++ src/main/java/com/technogise/leave_management_system/repository/LeaveIntegrationEventRepository.java
@@ -1,9 +1,12 @@
 package com.technogise.leave_management_system.repository;
 
 import com.technogise.leave_management_system.entity.LeaveIntegrationEvent;
+import com.technogise.leave_management_system.enums.PlatformType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LeaveIntegrationEventRepository extends JpaRepository<LeaveIntegrationEvent, UUID> {
+    Optional<LeaveIntegrationEvent> findByLeaveIdAndPlatformAndDeletedAtIsNull(UUID leaveId, PlatformType platform);
 }
