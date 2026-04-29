5ed7b70cc6c5da2602fc686d910b752a5e67f617
RajDeshmukh2001 <rajdd61@gmail.com>
2026-04-29 10:46:14 +0530

    refactor(LMS-90): use LeaveConstants for activity mapping instead of hardcoded strings

--- src/main/java/com/technogise/leave_management_system/constants/LeaveConstants.java
+++ src/main/java/com/technogise/leave_management_system/constants/LeaveConstants.java
@@ -3,4 +3,9 @@ package com.technogise.leave_management_system.constants;
 public class LeaveConstants {
     private LeaveConstants() { }
     public static final String ANNUAL_LEAVE = "Annual Leave";
+    public static final String PATERNITY_LEAVE = "Paternity Leave";
+    public static final String MATERNITY_LEAVE = "Maternity Leave";
+    public static final String SABBATICAL_LEAVE = "Sabbatical Leave";
+    public static final String BEREAVEMENT_LEAVE = "Bereavement Leave";
+    public static final String REJUVENATION_LEAVE = "5 Year Rejuvenation Leave";
 }
