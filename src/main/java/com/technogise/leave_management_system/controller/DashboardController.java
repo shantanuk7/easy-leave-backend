d3364844ae29086ac672ee288d497a57c47dd34f
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-30 13:21:29 +0530

    feat(lms-91): add GET /api/dashboard/employee employee dashboard API with logging

--- src/main/java/com/technogise/leave_management_system/controller/DashboardController.java
+++ src/main/java/com/technogise/leave_management_system/controller/DashboardController.java
@@ -1,14 +1,19 @@
 package com.technogise.leave_management_system.controller;
 
+import com.technogise.leave_management_system.dto.EmployeeAnnualLeaveSummaryResponse;
 import com.technogise.leave_management_system.dto.EmployeeMetricsResponse;
+import com.technogise.leave_management_system.entity.User;
 import com.technogise.leave_management_system.response.SuccessResponse;
 import com.technogise.leave_management_system.service.DashboardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/dashboard")
 public class DashboardController {
@@ -24,6 +29,20 @@ public class DashboardController {
         EmployeeMetricsResponse dashboardResponse = dashboardService.getManagerDashboardData();
         return ResponseEntity.ok(
                 SuccessResponse.success("Manager Dashboard data retrieved successfully", dashboardResponse)
+        );
+    }
+
+    @GetMapping("/employee")
+    public ResponseEntity<SuccessResponse<EmployeeAnnualLeaveSummaryResponse>> getEmployeeDashboardMetrics(
+            @AuthenticationPrincipal User user
+    ) {
+        log.info("GET /api/dashboard/employee called by userId={}", user.getId());
+
+        EmployeeAnnualLeaveSummaryResponse dashboardResponse = dashboardService.getEmployeeDashboardData(user.getId());
+        log.debug("Returning dashboard info for userId={}, dashboard data = {}", user.getId(),dashboardResponse);
+
+        return ResponseEntity.ok(
+                SuccessResponse.success("Employee Annual Leave data retrieved successfully", dashboardResponse)
         );
     }
 }
