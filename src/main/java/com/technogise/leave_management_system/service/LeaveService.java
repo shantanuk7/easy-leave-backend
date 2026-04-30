79c1298bf1a8af0c10e39065ce036a0bae172a29
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-30 17:29:10 +0530

    refactor(LMS-79): remove unused logger

--- src/main/java/com/technogise/leave_management_system/service/LeaveService.java
+++ src/main/java/com/technogise/leave_management_system/service/LeaveService.java
@@ -479,6 +479,7 @@ public class LeaveService {
                 && leave.getLeaveCategory().getName().equals(LeaveConstants.ANNUAL_LEAVE)) {
             annualLeaveService.syncOnLeaveDeleted(leave.getUser(), leave.getDuration(), leave.getDate().getYear());
         }
+
         leaveIntegrationHandler.handleLeaveDelete(leave);
     }
 }
